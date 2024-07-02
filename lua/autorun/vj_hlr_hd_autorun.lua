07380c10d5179f68f6bbe454101022d3aedab5d5
DrVrej <DrVrej@users.noreply.github.com>
2024-07-02 02:58:33 -0400

    Update vj_hlr_hd_autorun.lua

--- lua/autorun/vj_hlr_hd_autorun.lua
+++ lua/autorun/vj_hlr_hd_autorun.lua
@@ -11,7 +11,7 @@ local AddonType = "NPC"
 
 local VJExists = file.Exists("lua/autorun/vj_base_autorun.lua", "GAME")
 if VJExists == true then
-	include('autorun/vj_controls.lua')
+	include("autorun/vj_controls.lua")
 	
 	local spawnCategory = "HL Resurgence: HD"
 	VJ.AddCategoryInfo(spawnCategory, {Icon = "vj_hl/icons/hl1_hd.png"})
